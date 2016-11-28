5bea22ab5f763b4a1c6c6b963786307132625482
Sam Heutmaker <sam@sam.local>
2016-11-28 12:08:46 -0800

    Init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+let userRouter = express.Router()
+
+userRouter.get('*', (req, res) => {
+	res.status(200).json({hello: 'world'})
+});
+
+app.use(userRouter)
+
+app.listen(PORT, () => {
+	console.log('App Live on port ' + PORT)
+});

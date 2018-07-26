1fc156fc4f996415d66b846788962b3a90597534
Walter Gildersleeve <32897170+wgilder@users.noreply.github.com>
2018-07-26 09:19:44 +0200

    minor change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 let userRouter = express.Router()
 
 userRouter.get('*', (req, res) => {
-	res.status(200).json({hello: 'world'})
+	res.status(200).json({hello: 'world',goodbye: 'world'})
 });
 
 app.use(userRouter)
